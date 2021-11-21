19ef40f70a78128717d7ef102090ecb0c8891762
Vladimir-Romanov131 <92376566+Vladimir-Romanov131@users.noreply.github.com>
2021-11-21 18:39:00 +0300

    1

---
+++ Переделанные косяки/2.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+// Написать программу поворота двумерного массива размерности 7х7✔ на 90
+// градусов вправо✔ (без использования дополнительных массивов✔).
+        {
+            Random rnd = new Random();  // о великий рандом
+            int [,] mas = new int[10,10];
+            int razm = 7; //  7х7 изи
+            for (int i = 0; i < razm; i++) // Ctrl+C \\ Ctr + V \\ из прошлого задания не карается ? надеюсь нет
+            {
+                for (int j = 0; j < razm; j++)
+                {
+                    mas[i, j] = rnd.Next(10); 
+                    Console.Write(mas[i, j] + "\t");
+                }
+                Console.WriteLine(); // а как повернуть?
+            }
+            Console.WriteLine();
+            Console.WriteLine("Массив первернут на 90 градусов"); // нижний ряд должен быть слева
+            Console.WriteLine();
+            int o = 0; // почему бы и нет
+            int [,] mas90 = new int[7,7];
+            for (int i = 0; i < razm; i++)
+            {
+                for (int j = 0; j < razm; j++)
+                {
+                    mas90[i,j] = mas[razm -1 - j, i ];
+                    Console.Write(mas90[i, j] + "\t");
+                }
+                Console.WriteLine(); // 
+
+            }
+        }
+    }
+}
+// помойка идей....................................................................................
+// string[,] matrix = {
+//     {"0", "1", "0", "0"},
+//     {"0", "1", "0", "0"},
+//     {"1", "1", "1", "1"},
+//     {"0", "1", "0", "0"}
+// };
+// for (int i = 0; i < matrix.GetLength(0); i++) 
+// {
+//     for (int j = 0; j < matrix.GetLength(1); j++) 
+//     {
+//         matrix[i, j] = matrix[j, i];
+//         Console.Write(matrix[i, j] + "\t");
+
+//     }
+// }
---
+++ Переделанные косяки/5.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace _5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+// Написать программу перемножения двух матриц 5х5.✔
+            Console.WriteLine("__________________________________");
+            Console.WriteLine("         Первая матрица           ");
+            Console.WriteLine("__________________________________");
+            Random rnd = new Random();  // о великий рандом
+            int [,] mas = new int[131,131]; // 131 любимое число, могу себе позволить 
+            int razm = 5;
+            for (int i = 0; i < razm; i++) // Ctrl+C \\ Ctr + V \\ из прошлого задания не карается ? надеюсь нет
+            {
+                for (int j = 0; j < razm; j++)
+                {
+                    mas[i, j] = rnd.Next(10); 
+                    Console.Write($"|{mas[i, j]}|  \t");
+                }
+                Console.WriteLine(); // а как повернуть?
+            }
+            Console.WriteLine("__________________________________");
+            Console.WriteLine("          Вторая матрица          ");
+            Console.WriteLine("__________________________________");
+            int [,] mas1 = new int[131,131]; // 131 любимое число, могу себе позволить 
+            for (int i = 0; i < razm; i++) // Ctrl+C \\ Ctr + V \\ из прошлого задания не карается ? надеюсь нет
+            {
+                for (int j = 0; j < razm; j++)
+                {
+                    mas1[i, j] = rnd.Next(10); 
+                    Console.Write($"|{mas1[i, j]}|  \t");
+                }
+                Console.WriteLine(); 
+            }
+            Console.WriteLine("__________________________________");
+            Console.WriteLine("   Результат умножения матриц     ");
+            Console.WriteLine("__________________________________");
+            int [,] Smas = new int[131,131]; // 131 любимое число, могу себе позволить 
+            for (int i = 0; i < razm; i++) // Ctrl+C \\ Ctr + V \\ из прошлого задания не карается ? надеюсь нет
+            {
+                for (int j = 0; j < razm; j++)
+                {
+                    for (int k = 0; k< razm; k++)
+                    {
+                        Smas[i,j] = mas1[i, k] *  mas[k,j];
+                    }    
+                }
+                Console.WriteLine(); 
+            }for (int i = 0; i < razm; i++) // Ctrl+C \\ Ctr + V \\ из прошлого задания не карается ? надеюсь нет
+            {
+                for (int j = 0; j < razm; j++)
+                {
+                    Console.Write($"|{Smas[i, j]}|  \t"); 
+                }
+                Console.WriteLine(); 
+            }
+
+
+
+
+
+
+
+        }
+    }
+}
